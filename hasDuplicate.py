c2638bddae48e1698139352054006caf17ba8ee8
ezdimitry <137773973+ezdimitry@users.noreply.github.com>
2024-08-16 11:34:09 +0300

    add hasDublicate

---
+++ hasDuplicate.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        seen = set()
+        for i in nums:
+            if i in seen:
+                return True
+            seen.add(i)
+        return False
+
+
+sol = Solution()
+nums = [1, 2, 3, 4]
+print(sol.containsDuplicate(nums))
